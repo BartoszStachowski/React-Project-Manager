b5eff314557934e0110c1c81f3a793726c5432f3
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-05-18 13:45:07 +0200

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { ProjectSidebar } from "./components/ProjectSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="mt-2 text-center text-2xl font-bold">My react app</h1>
-    </>
+    <main>
+      <ProjectSidebar />
+    </main>
   );
 }
 
---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,9 @@
+export const ProjectSidebar = () => {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <button>+ Add Project</button>
+      <ul>...</ul>
+    </aside>
+  );
+};
