cc510eb9001180e99a2c38975bdee36ded3b1cd7
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-05-19 10:15:53 +0200

    include NewProject component into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { NewProject } from "./components/NewProject";
 import { ProjectSidebar } from "./components/ProjectSidebar";
 
 function App() {
   return (
-    <main className="my-8 h-screen">
+    <main className="my-8 flex h-screen gap-8">
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
