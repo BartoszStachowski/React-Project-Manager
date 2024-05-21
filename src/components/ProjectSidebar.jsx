6d7ea33a0060d0481f8debf6600d686dce53c810
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-05-21 17:08:46 +0200

    attaching a button component to ProjectSidebar

--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,12 +1,12 @@
+import { Button } from "./Button";
+
 export const ProjectSidebar = () => {
   return (
     <aside className="w-1/3 rounded-r-xl bg-stone-900 px-8 py-16 text-stone-50 md:w-72">
       <h2 className="mb-8 font-bold uppercase text-stone-200 md:text-xl">
         Your Projects
       </h2>
-      <button className="rounded-md bg-stone-700 px-4 py-2 text-xs text-stone-400 hover:bg-stone-600 hover:text-stone-100 md:text-base">
-        + Add Project
-      </button>
+      <Button>+ Add Project</Button>
       <ul>...</ul>
     </aside>
   );
