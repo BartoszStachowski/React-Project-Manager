62d24c2182b1f9834d78eab0cb72798e3f5d5319
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-05-22 18:38:55 +0200

    switch between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import { NewProject } from "./components/NewProject";
+import { NoProjectSelected } from "./components/NoProjectSelected";
 import { ProjectSidebar } from "./components/ProjectSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="my-8 flex h-screen gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.webp";
 import { Button } from "./Button";
 
-export const NoProjectSelected = () => {
+export const NoProjectSelected = ({ onStartAddProject }) => {
   return (
     <div className="mt-24 w-2/3 text-center">
       <img
@@ -16,7 +16,7 @@ export const NoProjectSelected = () => {
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
