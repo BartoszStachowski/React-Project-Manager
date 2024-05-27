8e3e8e57ac797907de5f715849e5231f79646297
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-05-27 17:06:00 +0200

    preview of the selected project

---
+++ src/components/SelectedProject.jsx
@@ -0,0 +1,27 @@
+export const SelectedProject = ({ project }) => {
+  const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  return (
+    <div className="mt-16 w-[35rem]">
+      <header className="mb-4 border-b-2 border-stone-300 pb-4">
+        <div className="flex items-center justify-between">
+          <h1 className="mb-2 text-3xl font-bold text-stone-600">
+            {project.title}
+          </h1>
+          <button className="text-stone-600 hover:text-stone-950">
+            Delete
+          </button>
+        </div>
+        <p className="mb-4 text-stone-400">{formattedDate}</p>
+        <p className="whitespace-pre-wrap text-sky-600">
+          {project.description}
+        </p>
+      </header>
+      TASKS
+    </div>
+  );
+};
